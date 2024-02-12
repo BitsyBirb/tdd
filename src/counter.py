9d5f7edfbb37fb1a3851ac2d93ec29759f9837a5
Miguel A. <maau70325@gmail.com>
2024-02-12 00:58:18 -0800

    Double checking to see if it all works on my fork.

---
+++ src/counter.py
@@ -0,0 +1,44 @@
+from flask import Flask
+app = Flask(__name__)
+
+from src import status
+
+COUNTERS = {}
+
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+
+# Route as put method
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """Update a counter"""
+    # Create a route for method PUT on endpoint /counters/<name>
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+    # Make sure the counter exists
+    if name not in COUNTERS:
+        return {"Message":f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
+    # Create a function to implement that route
+    # Increment the counter by 1
+    COUNTERS[name] += 1
+    # Return the new counter and a 200_OK return code.
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+
+@app.route('/counters/<name>', methods=['GET'])
+def get_counter(name):
+    """Should get a counter"""
+    # Very similar to update without the increment I think
+    app.logger.info(f"Request to get counter: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        return {"Message":f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,67 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+from unittest import TestCase
+
+from src.counter import app
+
+from src import status
+
+# Shoudl be in counter.py instead
+#from flask import Flask
+#app = Flask(__name__)
+
+
+class CounterTest(TestCase):
+    """Counter Tests"""
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        """It should create a counter."""
+        #client = app.test_client()
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """Should successfully update a counter"""
+        # Make a call to create a counter
+        result = self.client.post('/counters/test')
+        # Ensure that it returned a successful value code
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        # Check the counter value as a baseline
+        self.assertEqual(result.json['test'], 0)
+        # Make a call to update the counter that we just created
+        toCompare = self.client.put('/counters/test')
+        # Ensure that it returned a successful return code
+        self.assertEqual(toCompare.status_code, status.HTTP_200_OK)
+        # Check that the counter value is one more than the baseline measured in step 3
+        self.assertEqual(result.json['test'] + 1, toCompare.json['test'])
+
+    def test_get_a_counter(self):
+        """Should successfully get a counter"""
+        result = self.client.post('/counters/getTest')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        retrieved = self.client.get('/counters/getTest')
+        self.assertEqual(retrieved.status_code, status.HTTP_200_OK)
+        # Also want to make sure that the value in the counter itself is 0 as it was just made, compare to OG
+        self.assertEqual(retrieved.json['getTest'], result.json['getTest'])
+
+    def test_updating_nonexistent_counter(self):
+        """Should successfully get 404 on an attempt to update a missing counter"""
+        result = self.client.put('/counters/narnia')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_getting_nonexistent_counter(self):
+        """Should successfully return 404 status on attempt to get missing counter"""
+        result = self.client.get('counters/narnia')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
