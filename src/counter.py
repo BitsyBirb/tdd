4358fe39e2b358ddfd360e94808f4c05b48a2969
Miguel A. <maau70325@gmail.com>
2024-02-12 13:49:29 -0800

    Okay this commit should solve the linter issues with counter.py.

--- src/counter.py
+++ src/counter.py
@@ -8,26 +8,26 @@ COUNTERS = {}
 
 
 @app.route('/counters/<name>', methods=['POST'])
-def create_counter(name): 
+def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
     global COUNTERS
     if name in COUNTERS:
-        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"Message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
 
 # Route as put method
 @app.route('/counters/<name>', methods=['PUT'])
-def update_counter(name): 
+def update_counter(name):
     """Update a counter"""
     # Create a route for method PUT on endpoint /counters/<name>
     app.logger.info(f"Request to update counter: {name}")
     global COUNTERS
     # Make sure the counter exists
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
     # Create a function to implement that route
     # Increment the counter by 1
     COUNTERS[name] += 1
@@ -36,11 +36,11 @@ def update_counter(name):
 
 
 @app.route('/counters/<name>', methods=['GET'])
-def get_counter(name): 
+def get_counter(name):
     """Should get a counter"""
     # Very similar to update without the increment I think
     app.logger.info(f"Request to get counter: {name}")
     global COUNTERS
     if name not in COUNTERS:
-        return {"Message":f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
+        return {"Message": f"Counter {name} doesn't exist"}, status.HTTP_404_NOT_FOUND
     return {name: COUNTERS[name]}, status.HTTP_200_OK
